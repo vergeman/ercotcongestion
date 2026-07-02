6bbef095c2ff4d3ef0dbc3969b560977bd75b755
vergeman <alan.verga@gmail.com>
2026-07-01 21:40:58 -0500

    refactor(ops): replace hardcoded version with git tag

--- ops/deploy/deploy-api.sh
+++ ops/deploy/deploy-api.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/bash
 
 export $(grep -E '^(IMAGE_REPO)' ../../.env)
-envsubst '${IMAGE_REPO}' < base/api/deploy.yml | kubectl apply -f -
+export IMAGE_TAG="$(cat ../../.image-tag)"
+envsubst '${IMAGE_REPO} ${IMAGE_TAG}' < base/api/deploy.yml | kubectl apply -f -
 
 kubectl apply -f base/api/service.yml
 kubectl apply -f base/cert-issuer.yml
